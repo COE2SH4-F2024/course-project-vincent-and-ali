5308b84979aaa054bce91ddfcf5414930344c223
yuw79 <yuw79@mcmaster.ca>
2024-11-19 20:13:01 -0500

    copied and pasted draw routine

--- Project.cpp
+++ Project.cpp
@@ -52,10 +52,48 @@ void RunLogic(void)
 {
     
 }
-
+//Copied and Pasted from PPA3
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+    int i, j, k;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 20; j++)
+        {
+            if (i == 0 || i == 10 - 1 || j == 0 || j == 20 - 1)
+            {
+                MacUILib_printf("#");
+            }
+            else if (i == 3 && j == 5)
+            {
+                MacUILib_printf("+");
+            }
+            
+            // else if (i == player.y && j == player.x)
+            // {
+            //     MacUILib_printf("%c", player.symbol);
+            // }
+            // else
+            // {
+            //     int foundItem = 0;
+            //     for (k = 0; k < 5; k++)
+            //     {
+            //         if (i == itemList[k].y && j == itemList[k].x)
+            //         {
+            //             MacUILib_printf("%c", itemList[k].symbol);
+            //             foundItem = 1;
+            //             break;
+            //         }
+            //     }
+            else 
+            {
+                MacUILib_printf(" ");
+            }
+            // } 
+        }
+        MacUILib_printf("\n");
+    }    
 }
 
 void LoopDelay(void)
